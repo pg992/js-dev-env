f06532ae1b4fe8d4ff92099b6bdea46bf18d16c5
pg992 <Peter.Georgiev@wowinc.com>
2017-12-25 00:24:42 +0100

    Adding bundling, sourcemaps

---
+++ src/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+import numeral from 'numeral';
+
+const courseValue = numeral(1000).format('$0,0.00');
+debugger;
+console.log(`I would pay ${courseValue} for this awesome course!`); // `` -> ES6 template string feature
---
+++ webpack.config.dev.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import path from 'path';
+
+export default {
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web', // node, electron
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
+      {test: /\.css$/, loaders: ['style', 'css']}
+    ]
+  }
+}
