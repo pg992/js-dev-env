bb35d18c0f9f663af6018ddc98bcc6042844cf9e
pg992 <Peter.Georgiev@wowinc.com>
2017-12-25 01:48:49 +0100

    Continious integration and testing intial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 /* eslint-disable no-console */
 
 import numeral from 'numeral';
+// import './index.css'
 
 const courseValue = numeral(1000).format('$0,0.00');
 // debugger;
---
+++ src/index.test.js
@@ -0,0 +1,20 @@
+import {expect} from 'chai';
+import {JSDOM} from 'jsdom';
+
+describe('Our first test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe('index.html', () => {
+  it('should say hello', (done) => {
+      JSDOM.fromFile('./src/index.html').catch(function(err) {
+        console.error("Error apears: " + err); // eslint-disable-line no-console
+      }).then(dom => {
+        const h1 = dom.window.document.getElementsByTagName('h1')[0];
+        expect(h1.innerHTML).to.equal("Hello World!!");
+        done();
+      });
+  });
+});
